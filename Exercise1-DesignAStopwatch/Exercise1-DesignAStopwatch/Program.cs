a1cef0b0a93dc2df55bbec585d87ff603521a40e
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2019-09-08 19:58:55 -0700

    Exercise 1: Design a Stopwatch
    Design a class called Stopwatch. The job of this class is to simulate a stopwatch. It should provide two methods: Start and Stop. We call the start method first, and the stop method next. Then we ask the stopwatch about the duration between start and stop. Duration should be a value in TimeSpan. Display the duration on the console.
    
    We should also be able to use a stopwatch multiple times. So we may start and stop it and then start and stop it again. Make sure the duration value each time is calculated properly.
    
    We should not be able to start a stopwatch twice in a row (because that may overwrite the initial start time). So the class should throw an InvalidOperationException if its started twice.

---
+++ Exercise1-DesignAStopwatch/Exercise1-DesignAStopwatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+
+namespace Exercise1_DesignAStopwatch
+{
+    /*
+     * Design a class called Stopwatch.
+     * The job of this class is to simulate a stopwatch.
+     * It should provide two methods: Start and Stop.
+     * We call the start method first, and the stop method next.
+     * Then we ask the stopwatch about the duration between start and stop.
+     * Duration should be a value in TimeSpan.
+     * Display the duration on the console.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a stopwatch object
+            var stopwatch = new Stopwatch();
+
+            // Check initial state of stopwatch
+            Console.WriteLine("Time Elapsed: " + stopwatch.Duration);
+
+            // Run the stopwatch fpr 1 second
+            stopwatch.Start();
+            Thread.Sleep(1000);
+            stopwatch.End();
+
+            // Display time elapsed
+            Console.WriteLine("Time Elapsed: " + stopwatch.Duration);
+            
+            // Reset the stopwatch
+            stopwatch.Reset();
+
+            // Display time elapsed
+            Console.WriteLine("Time Elapsed: " + stopwatch.Duration);
+
+            // Start the stopwatch twice
+            stopwatch.Start();
+            stopwatch.Start();
+
+            // Run the stopwatch for 1 millisecond
+            Thread.Sleep(1);
+
+            // End the stopwatch twice
+            stopwatch.End();
+            stopwatch.End();
+
+            // Display time elapsed
+            Console.WriteLine("Time Elapsed: " + stopwatch.Duration);
+
+            // Reset  the stopwatch
+            stopwatch.Reset();
+
+            // Display time elapsed
+            Console.WriteLine("Time Elapsed: " + stopwatch.Duration);
+        }
+    }
+}
---
+++ Exercise1-DesignAStopwatch/Exercise1-DesignAStopwatch/Stopwatch.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Exercise1_DesignAStopwatch
+{
+    class Stopwatch
+    {
+        private DateTime _startTime;                        // Start Time
+        private DateTime _endTime;                          // End Time
+        public TimeSpan Duration { get; private set; }      // Active Time Duration
+        private bool _inUse;                                // Stopwatch in use flag
+
+        /*
+         * --- Constructor ---
+         * Used to reset the stopwatch before initial use.
+         */
+        public Stopwatch()
+        {
+            // Reset the stopwatch
+            Reset();
+        }
+
+        /*
+         * --- Start ---
+         * Used to start the stopwatch if not in use.
+         */
+        public void Start()
+        {
+            try
+            {
+                if (_inUse)
+                {
+                    throw new InvalidOperationException("_inUse");
+                }
+
+                _startTime = DateTime.Now;
+                _inUse = true;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("The stopwatch has already been started.");
+                return;
+            }
+        }
+
+        /*
+         * --- End ---
+         * Used to stop the stopwatch if in use.
+         */
+        public void End()
+        {
+            try
+            {
+                if (!_inUse)
+                {
+                    throw new InvalidOperationException("_inUse");
+                }
+
+                _endTime = DateTime.Now;
+                Duration = _endTime - _startTime;
+                _inUse = false;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("The stopwatch has not yet been started.");
+            }
+        }
+
+        /*
+         * --- Reset ---
+         * Used to set the stopwatch to an initial ready state
+         */
+        public void Reset()
+        {
+            _startTime = DateTime.Now;
+            _endTime = _startTime;
+            Duration = TimeSpan.Zero;
+            _inUse = false;
+        }
+    }
+}
