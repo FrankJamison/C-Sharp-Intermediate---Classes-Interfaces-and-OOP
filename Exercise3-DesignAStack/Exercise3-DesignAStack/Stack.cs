d3a3dae5a33b039777046e42da28c26be8374dd7
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2019-09-08 21:08:20 -0700

    Exercise 3: Design a Stack
    Design a class called Stack with three methods.
    
    void Push(object obj)
    object Pop()
    void Clear()
    
    The Push() method stores the given object on top of the stack. We use the “object” type here so we can store any objects inside the stack. Remember the “object” class is the base of all classes in the .NET Framework. So any types can be automatically upcast to the object. Make sure to take into account the scenario that null is passed to this object. We should not store null references in the stack. So if null is passed to this method, you should throw an InvalidOperationException. Remember, when coding every method, you should think of all possibilities and make sure the method behaves properly in all these edge cases. That’s what distinguishes you from an “average” programmer.
    
    The Pop() method removes the object on top of the stack and returns it. Make sure to take into account the scenario that we call the Pop() method on an empty stack. In this case, this method should throw an InvalidOperationException. Remember, your classes should always be in a valid state and used properly. When they are misused, they should throw exceptions. Again, thinking of all these edge cases, separates you from an average programmer. The code written this way will be more robust and with less bugs.
    
    The Clear() method removes all objects from the stack.
    
    We should be able to use this stack class as follows:
    
    var stack = new Stack();
    stack.Push(1);
    stack.Push(2);
    stack.Push(3);
    
    Console.WriteLine(stack.Pop());
    Console.WriteLine(stack.Pop());
    Console.WriteLine(stack.Pop());
    
    The output of this program will be:
    3
    2
    1

---
+++ Exercise3-DesignAStack/Exercise3-DesignAStack/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3_DesignAStack
+{
+    class Stack
+    {
+        private Stack<object> _stack = new Stack<object>();     // Stack object
+
+        /*
+         * METHOD: Push
+         * Used to push an object on to the stack
+         */
+        public void Push(object obj)
+        {
+            try
+            {
+                // If the object being pushed is null, throw an exception
+                if (obj == null)
+                {
+                    throw new InvalidOperationException("obj");
+                }
+
+                // Push object on to the stack
+                _stack.Push(obj);
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("The item being pushed to the stack cannot be null");
+            }
+        }
+
+        /*
+         *  METHOD: Pop
+         * Used to remove an item from the stack
+         */
+        public object Pop()
+        {
+            try
+            {
+                // If the stack is empty, throw an exception
+                if (_stack.Count == 0)
+                {
+                    throw new InvalidOperationException("count");
+                }
+
+                // Remove item from the stack
+                return _stack.Pop();
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("You can't remove items from an empty stack.");
+                return null;
+            }
+        }
+
+        /*
+         * METHOD: Clear
+         * Used to empty the stack of all items
+         */
+        public void Clear()
+        {
+            // Remove all items from the stack
+            _stack.Clear();
+        }
+    }
+}
