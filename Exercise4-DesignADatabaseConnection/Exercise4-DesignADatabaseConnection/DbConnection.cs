1223202e4f08e0a21a152723b391b7e7dd5f9cfd
Frank Jamison <42881082+FrankJamison@users.noreply.github.com>
2019-09-08 22:37:27 -0700

    Exercise 4: Design a database connection
    To access a database, we need to open a connection to it first and close it once our job is done. Connecting to a database depends on the type of the target database and the database management system (DBMS). For example, connecting to a SQL Server database is different from connecting to an Oracle database. But both these connections have a few things in common:
    
    • They have a connection string
    • They can be opened
    • They can be closed
    • They may have a timeout attribute (so if the connection could not be opened within the timeout, an exception will be thrown).
    
    Your job is to represent these commonalities in a base class called DbConnection. This class should have two properties:
    
    ConnectionString : string
    Timeout : TimeSpan
    
    A DbConnection will not be in a valid state if it doesn’t have a connection string. So you need to pass a connection string in the constructor of this class. Also, take into account the scenarios where null or an empty string is sent as the connection string. Make sure to throw an exception to guarantee that your class will always be in a valid state.
    
    Our DbConnection should also have two methods for opening and closing a connection. We don’t know how to open or close a connection in a DbConnection and this should be left to the classes that derive from DbConnection. These classes (eg SqlConnection or OracleConnection) will provide the actual implementation. So you need to declare these methods as abstract.
    
    Derive two classes SqlConnection and OracleConnection from DbConnection and provide a simple implementation of opening and closing connections using Console.WriteLine(). In the real-world, SQL Server provides an API for opening or closing a connection to a database. But for this exercise, we don’t need to worry about it.

---
+++ Exercise4-DesignADatabaseConnection/Exercise4-DesignADatabaseConnection/DbConnection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise4_DesignADatabaseConnection
+{
+    abstract class DbConnection
+    {
+        protected string ConnectionString;      // Connection string
+        private TimeSpan Timeout;               // Connection timeout period
+
+        /*
+         * --- CONSTRUCTOR ---
+         * Constructor requires a connection string and sets timeout to 30 seconds
+         */
+        public DbConnection(string connectionString)
+        {
+            try
+            {
+                // If the connection string is null or whitespace, throw an exception.
+                if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    throw new InvalidOperationException("connectionString");
+                }
+
+                // Set the connection string value
+                ConnectionString = connectionString;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("The database connection string cannot be null or whitespace.");
+            }
+
+            // Set timeout to 30 seconds
+            Timeout = new TimeSpan(0,0,30);
+        }
+
+        // Abstract methods that will be overridden by derived classes
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+    }
+}
---
+++ Exercise4-DesignADatabaseConnection/Exercise4-DesignADatabaseConnection/SqlConnection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercise4_DesignADatabaseConnection
+{
+    class SqlConnection : DbConnection
+    {
+        private bool SqlConnectionOpen;
+
+        /*
+         * --- CONSTRUCTOR: SqlConnection ---
+         * Requires: connection string
+         * Sets: Connection open flag
+         */
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+            // Set the SQL Connection Open flag to false
+            SqlConnectionOpen = false;
+        }
+
+        /*
+         * --- METHOD: OpenConnection ---
+         * Overrides method in DbConnection class
+         * Used to open an Oracle database connection
+         */
+        public override void OpenConnection()
+        {
+            try
+            {
+                // If a connection is already open, throw an ex exception
+                if (SqlConnectionOpen)
+                {
+                    throw new InvalidOperationException("SqlConnectionOpen");
+                }
+
+                // Open the connection
+                Console.WriteLine("Connecting to SQL database...");
+                SqlConnectionOpen = true;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("An SQL database connection is already open.");
+            }
+        }
+
+        /*
+         * --- METHOD: CloseConnection ---
+         * Overrides method in DbConnection class
+         * Used to close the Oracle database connection
+         */
+        public override void CloseConnection()
+        {
+            try
+            {
+                // If the connection is already closed, throw an exception
+                if (!SqlConnectionOpen)
+                {
+                    throw new InvalidOperationException("ConnectionOpen");
+                }
+
+                Console.WriteLine("Closing SQL database connection...");
+                SqlConnectionOpen = false;
+            }
+
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("An SQL database connection is not currently open.");
+            }
+        }
+    }
+}
